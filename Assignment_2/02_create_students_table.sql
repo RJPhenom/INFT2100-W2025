1320177234b689c590b6caac42654ca33d3da3ea
RJPhenom <103343995+RJPhenom@users.noreply.github.com>
2025-02-11 21:46:04 -0500

    Add comment headers two students and marks create files

---
+++ Assignment_2/02_create_students_table.sql
@@ -0,0 +1,12 @@
+/*
+Robert Macklem
+01_create_students_table.sql
+February 11, 2025
+INFT2100 Winter 2025
+
+Description:
+
+Create the students table, based on 
+Assignment #2's requirements
+
+*/
---
+++ Assignment_2/04_create_marks_table.sql
@@ -0,0 +1,12 @@
+/*
+Robert Macklem
+04_create_marks_table.sql
+February 11, 2025
+INFT2100 Winter 2025
+
+Description:
+
+Create the marks table, based on 
+Assignment #2's requirements
+
+*/
