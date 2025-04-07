a555a99afae0bba4a5d454853bcb6f1e0ee368a4
RJ <rj@macklem.ca>
2025-04-07 18:28:24 -0400

    Removed outdated comment

--- logout.php
+++ logout.php
@@ -9,9 +9,6 @@
 * @version 1.0 (April 5, 2025)
 */
 
-// I dont include header.php cuz we dont actually visit this page
-// and there is no html needed.
-
 $file = "logout.php";
 $description = "Logout page for my INFT2100 Site.";
 $date = "April 5, 2025";
